761eaf23e062f4b5c192cb4623bd88e8ffe5258b
Nallen14 <sow.niazia@yahoo.com>
2020-08-20 15:10:17 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';  
+import ReactDOM from 'react-dom';  
+  
+class App extends React.Component {  
+  render() {  
+    return (  
+      <div>  
+      <h1 style={{color: "Red"}}>Hello JavaTpoint!</h1>  
+      <p style={{backgroundColor: "lightgreen"}}>Here, you can find all CS tutorials.</p>  
+      </div>  
+    );  
+  }  
+}  
+export default App;  
